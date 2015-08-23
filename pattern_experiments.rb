8369900d9c338e9e8051a4d40985fd2a223bf009
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-23 09:03:22 -0700

    sample module & class

--- pattern_experiments.rb
+++ pattern_experiments.rb
@@ -1,3 +1,34 @@
 # I am going to try to implement syntax for elixir like pattern matching across functions in Ruby
 # Some ideas for implementation:
 # __DATA__ & __END__ w/ eval?
+
+module Pattern
+  def match(&block)
+    p "hi"           # => "hi"
+  end
+end
+
+class User
+  extend Pattern  # => User
+
+  def initialize
+  end
+
+  match do
+    [%q(def fib(0)
+      0
+    end)]
+
+    [%q(def fib(1)
+      1
+    end)]
+
+    [%q(def fib(x)
+      fib(x-2) + fib(x-1)
+    end)]
+  end      # => "hi"
+end
+
+User.new  # => #<User:0x007ffa4991a550>
+
+# >> "hi"
