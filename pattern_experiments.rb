e41603b39d5ec02111226723d0be674f21285a7e
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-23 09:44:32 -0700

    regex for each method and eval

--- pattern_experiments.rb
+++ pattern_experiments.rb
@@ -5,9 +5,11 @@
 
 module Pattern
   def match(&block)
-    block.call.map do |method|  # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
-      method.gsub("\n", "; ")    # => "def fib(0);       0;     end", "def fib(1);       1;     end", "def fib(x);       fib(x-2) + fib(x-1);     end"
-    end                          # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
+    block.call.map do |method|                       # => [["def fib(0)", 0, "end"], ["def fib(1)", 1, "end"], ["def fib(:x)", "fib(x-2) + fib(x-1)", "end"]]
+      var = method.first                             # => "def fib(0)", "def fib(1)", "def fib(:x)"
+      .match(/\(.*\)/).to_s.delete("(").delete(")")  # => "0", "1", ":x"
+      eval(var) if var.instance_of? String           # => 0, 1, :x
+    end                                              # => [0, 1, :x]
   end
 end
 
@@ -15,18 +17,18 @@ class User
   extend Pattern  # => User
 
   match do
-    [%q(def fib(0)
-      0
-    end),   # => "def fib(0)\n      0\n    end"
+    [["def fib(0)",  # => "def fib(0)"
+      0,             # => 0
+    "end"],          # => ["def fib(0)", 0, "end"]
 
-    %q(def fib(1)
-      1
-    end),  # => "def fib(1)\n      1\n    end"
+     ["def fib(1)",  # => "def fib(1)"
+       1,            # => 1
+     "end"],         # => ["def fib(1)", 1, "end"]
 
-    %q(def fib(x)
-      fib(x-2) + fib(x-1)
-    end)]  # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
-  end      # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
+     ["def fib(:x)",           # => "def fib(:x)"
+       "fib(x-2) + fib(x-1)",  # => "fib(x-2) + fib(x-1)"
+     "end"]]                   # => [["def fib(0)", 0, "end"], ["def fib(1)", 1, "end"], ["def fib(:x)", "fib(x-2) + fib(x-1)", "end"]]
+  end                          # => [0, 1, :x]
 end
 
-User.new  # => #<User:0x007ff8038a7458>
+User.new  # => #<User:0x007ff1f30e8908>
