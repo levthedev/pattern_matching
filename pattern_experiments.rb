ad739400236809d516568580bd2c8b72b9c17998
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-23 09:12:33 -0700

    parsing block into strings that can be evaluated

--- pattern_experiments.rb
+++ pattern_experiments.rb
@@ -5,26 +5,28 @@
 
 module Pattern
   def match(&block)
-
+    block.call.map do |method|  # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
+      method.gsub("\n", "; ")    # => "def fib(0);       0;     end", "def fib(1);       1;     end", "def fib(x);       fib(x-2) + fib(x-1);     end"
+    end                          # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
   end
 end
 
 class User
-  extend Pattern
+  extend Pattern  # => User
 
   match do
     [%q(def fib(0)
       0
-    end)]
+    end),   # => "def fib(0)\n      0\n    end"
 
-    [%q(def fib(1)
+    %q(def fib(1)
       1
-    end)]
+    end),  # => "def fib(1)\n      1\n    end"
 
-    [%q(def fib(x)
+    %q(def fib(x)
       fib(x-2) + fib(x-1)
-    end)]
-  end
+    end)]  # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
+  end      # => ["def fib(0)\n      0\n    end", "def fib(1)\n      1\n    end", "def fib(x)\n      fib(x-2) + fib(x-1)\n    end"]
 end
 
-User.new
+User.new  # => #<User:0x007ff8038a7458>
